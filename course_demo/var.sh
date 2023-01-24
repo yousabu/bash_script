31f0135dc837edd791a0641988fe258fa6db242c
Youssef <shbanmhmdywsf8@gmail.com>
2023-01-24 15:10:45 +0200

    sec2

--- course_demo/var.sh
+++ course_demo/var.sh
@@ -4,3 +4,11 @@ name=ZiZe
 
 # print name with lower case first letter
 echo ${name,}
+# print all word in lower case
+echo ${name,,}
+
+# Frist letter in upper case
+echo ${name^}
+
+# all word in Upper case
+echo ${name^^}
